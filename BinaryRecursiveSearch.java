2a3e47190eb721c8c43d6ba2a9fc9e25bead5982
Salima Mukhit <smukhit@dons.usfca.edu>
2020-02-12 13:52:08 -0800

    Update recursive binary search

--- BinaryRecursiveSearch.java
+++ BinaryRecursiveSearch.java
@@ -2,20 +2,19 @@ public class BinaryRecursiveSearch implements Practice03Search {
 
     @Override
     public int search(int[] arr, int target) {
-        return binary(arr, 0, arr.length-1, target);
+        return this.binary(arr, 0, arr.length-1, target);
     }
 
     int binary(int[] arr, int start, int end, int target) {
         if (end >= start) {
             int mid = (end - start) + start / 2;
-
-            if (arr[mid] == target)
+            if (arr[mid] == target) {
                 return mid;
-
-            if (arr[mid] > target)
+            } else if (arr[mid] > target) {
                 return binary(arr, start, mid - 1, target);
-
-            return binary(arr, mid + 1, end, target);
+            } else if (arr[mid] > target) {
+                return binary(arr, mid + 1, end, target);
+            }
         }
         return -1;
     }
