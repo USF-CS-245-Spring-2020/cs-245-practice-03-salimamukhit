ad7d5b2c2fd29dbe5cff23acc66687e8e0b950bc
Salima Mukhit <42685261+salimamukhit@users.noreply.github.com>
2020-02-12 20:34:25 -0800

    Properly commented

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,15 +2,15 @@ public class SelectionSort implements Practice03Sort {
 
     @Override
     public void sort(int[] a) {
-        int index;
-        int temp;
-        for (int i=0; i<a.length-1; i++) {
+        int index; // index of the lowest item
+        int temp; // variable for swapping
+        for (int i=0; i<a.length-1; i++) { // iterating through the array and choosing which indexes to replace
             index = i;
             for (int j=i+1; j<a.length; j++) {
-                if (a[j] < a[index]) {
+                if (a[j] < a[index]) { // updating the smallest value index
                     index = j;
                 }
-            }
+            } // swapping
             temp = a[i];
             a[i] = a[index];
             a[index] = temp;
